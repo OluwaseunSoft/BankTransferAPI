1e02118f5493141157200545a5eeae0ffff3004d
OluwaseunSoft <Oluwaseunazure@yahoo.com>
2025-03-04 10:34:48 +0100

    implementing-customer-types

--- Service/TransferService.cs
+++ Service/TransferService.cs
@@ -19,29 +19,53 @@ namespace BankTransactionAPI.Service
         public async Task<TransferResponse> DoTransfer(TransferRequest request)
         {
             var result = new TransferResponse();
+            var transactionData = new TransactionDataDto();
             try
             {
                 var account = await _account.GetAccount(request.SourceAccount);
-                if(account != null && account?.AccountNumber != "")
-                {
-                    var transactionData = new TransactionDataDto();
-                    transactionData.TransactionDate = DateTime.Now;
-                    transactionData.Rate = 0;
-                    transactionData.Amount = request.Amount;
-                    transactionData.AccountNumber = request.SourceAccount;
-                    transactionData.DiscountedAmount = 0;
-                    var doTransfer = await _transaction.SaveTransactionData(transactionData);
-                }
-                else
+                if (account == null || account?.AccountNumber == "")
                 {
                     throw new Exception("Invalid Source Account");
                 }
+                var customerType = await GetCustomerType(account.CustomerId);
+                if(customerType.CustomerType == "BUSINESS")
+                {
+                    transactionData.DiscountedAmount = await BusinessCustomerDiscount(account.CustomerId);
+                }
+                else if (customerType.CustomerType == "RETAIL")
+                {
+                    transactionData.DiscountedAmount = await RetailCustomerDiscount(account.AccountNumber);
+                }
+                
+                transactionData.TransactionDate = DateTime.Now;
+                transactionData.Rate = 0;
+                transactionData.Amount = request.Amount;
+                transactionData.AccountNumber = request.SourceAccount;
+                transactionData.DiscountedAmount = 0;
+                var doTransfer = await _transaction.SaveTransactionData(transactionData);
+
                 return result;
             }
             catch (Exception ex)
             {
                 throw new Exception(ex.Message);
             }
+        }
+
+        private async Task<CustomerDto> GetCustomerType(int customerId)
+        {
+            var customer = await _customer.GetCustomer(customerId.ToString());
+            return customer;
+        }
+
+        private async Task<int> BusinessCustomerDiscount(int customerId)
+        {
+            return 0;
+        }
+
+        private async Task<int> RetailCustomerDiscount(string accountNumber)
+        {
+            return 0;
         }
     }
 }
